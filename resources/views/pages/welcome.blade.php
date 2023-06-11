8d532b7d6dfa369e4513ca7b4d5677a2edb4b16b
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-11 11:22:43 +0200

    inserita section content con array di comics

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,4 +6,19 @@
 
 @section ( "content" )
 
+    <div class="container">
+        <div class="row">
+            @foreach( $comics as $elem )
+                <div class="col-4">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ $elem['thumb'] }}" alt="" srcset="">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $elem["series"] }}</h4>
+                            <span class="card-text">{{ $elem["price"] }}</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
